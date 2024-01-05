b9cbc61ad447bc9218d28a619611079fe8d37f3b
markvandenhoff <mark.vandenhoff@hotmail.com>
2024-01-05 16:56:42 +1100

    Commit the initial Repo

---
+++ server.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let app = express();
+let port = process.env.port || 3000;
+require('./dbConnection');
+let router = require('./routers/router');
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use('/api/cat',router);
+
+
+app.listen(port, ()=>{
+    console.log('express server started');
+});
